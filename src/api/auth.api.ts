6707833fdc8c1ebb78a526cd80aae833f363e1a9
gotechdevweb <vuhai91az@gmail.com>
2024-12-15 15:46:55 +0700

    Xóa toàn bộ code

--- src/api/auth.api.ts
+++
@@ -1,6 +0,0 @@
-import axios from "../axiosConfig";
-import { LoginType } from "../types/auth.type";
-
-export const loginApi = (body: LoginType) => {
-  return axios.post("http://localhost:4000/auth/login", body);
-};
--- src/types/post.type.ts
+++
@@ -1,8 +0,0 @@
-export type PostItem = {
-  description: string;
-  featuredImage: string;
-  id: string;
-  publishDate: string;
-  published: boolean;
-  title: string;
-};
