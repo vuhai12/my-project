cb6942d0955b84848f6033db2e0811b99aa88d12
gotechdevweb <vuhai91az@gmail.com>
2024-12-15 22:42:24 +0700

    Chỉnh sửa cấu hình Vercel và Vite

--- frontend_redux/vite.config.ts
+++ frontend_redux/vite.config.ts
@@ -4,10 +4,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist", // Thư mục build
+    outDir: "frontend_redux/dist", // Đảm bảo đường dẫn đúng
   },
   server: {
     port: 4000,
   },
-  base: "/", // Đường dẫn gốc
+  base: "/",
 });
